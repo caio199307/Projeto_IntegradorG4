ab107d9e4cddabd6f329321e75fa0e17b56d69d1
Anderson Correia de Souza <anderson_souza33@outlook.com.br>
2021-04-30 09:38:50 -0300

    start: Spring e dependencias

---
+++ crowdfunding/src/main/java/com/bio/crowdfunding/CrowdfundingApplication.java
@@ -0,0 +1,13 @@
+package com.bio.crowdfunding;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CrowdfundingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CrowdfundingApplication.class, args);
+	}
+
+}
---
+++ crowdfunding/src/test/java/com/bio/crowdfunding/CrowdfundingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bio.crowdfunding;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CrowdfundingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
