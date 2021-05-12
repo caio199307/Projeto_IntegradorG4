6b758d31312ba47165b6e20e8eb0e57a1632544f
Anderson Correia de Souza <anderson_souza33@outlook.com.br>
2021-05-12 10:34:50 -0300

    feat: camada de security feita

--- crowdfunding/src/main/java/com/bio/crowdfunding/repository/UsuarioRepository.java
+++ crowdfunding/src/main/java/com/bio/crowdfunding/repository/UsuarioRepository.java
@@ -1,5 +1,7 @@
 package com.bio.crowdfunding.repository;
 
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
@@ -7,5 +9,6 @@ import com.bio.crowdfunding.model.Usuario;
 
 @Repository
 public interface UsuarioRepository extends JpaRepository<Usuario, Long> {
-
+	//UserName neste caso foi igual ao email
+	Optional<Usuario> findByUsuario(String usuario);
 }
