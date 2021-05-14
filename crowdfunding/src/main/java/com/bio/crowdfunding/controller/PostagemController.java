6d08489fb5872b374046c958dd15144b492119cf
Camilla C <camillaoui@gmai.com>
2021-05-14 18:08:34 -0300

    feat: service de doacao

--- crowdfunding/src/main/java/com/bio/crowdfunding/controller/PostagemController.java
+++ crowdfunding/src/main/java/com/bio/crowdfunding/controller/PostagemController.java
@@ -1,6 +1,7 @@
 package com.bio.crowdfunding.controller;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
@@ -17,53 +18,68 @@ import org.springframework.web.bind.annotation.RestController;
 
 import com.bio.crowdfunding.model.Postagem;
 import com.bio.crowdfunding.repository.PostagemRepository;
+import com.bio.crowdfunding.service.PostagemService;
 
 import io.swagger.annotations.ApiOperation;
 
-
 @RestController
-@CrossOrigin(origins = "*" , allowedHeaders = "*")
+@CrossOrigin(origins = "*", allowedHeaders = "*")
 @RequestMapping("/postagem")
 public class PostagemController {
-	
+
 	@Autowired
 	private PostagemRepository repository;
 	
+	@Autowired
+	private PostagemService service;
+
 	@ApiOperation(value = "consulta de todas as postagens")
 	@GetMapping
-	public ResponseEntity<List<Postagem>> findAll(){
+	public ResponseEntity<List<Postagem>> findAll() {
 		return ResponseEntity.ok(repository.findAll());
 	}
-	
+
 	@ApiOperation(value = "consulta uma postagem através do id")
 	@GetMapping("/{id}")
 	public ResponseEntity<Postagem> findById(@PathVariable long id) {
 		return repository.findById(id).map(resp -> ResponseEntity.ok(resp)).orElse(ResponseEntity.notFound().build());
 	}
-	
+
 	@ApiOperation(value = "consulta de postagens através de parte de um título")
 	@GetMapping("/titulo/{titulo}")
-    public ResponseEntity<List<Postagem>> findByTitulo(@PathVariable String titulo){
-      return ResponseEntity.ok(repository.findAllByTituloContainingIgnoreCase(titulo));
-   }
-	
+	public ResponseEntity<List<Postagem>> findByTitulo(@PathVariable String titulo) {
+		return ResponseEntity.ok(repository.findAllByTituloContainingIgnoreCase(titulo));
+	}
+
 	@ApiOperation(value = "faz uma postagem")
 	@PostMapping
-	public ResponseEntity<Postagem> post(@RequestBody Postagem Postagem){
+	public ResponseEntity<Postagem> post(@RequestBody Postagem Postagem) {
 		return ResponseEntity.status(HttpStatus.CREATED).body(repository.save(Postagem));
 	}
-	
-	//SERÁ NECESSÁRIO COLOCAR DUAS FORMAS DE ALTERAR A POSTAGEM, UMA DO CRIADOR DO POST E OUTRA PARA OS OUTROS USUÁRIOS.
+
+	// SERÁ NECESSÁRIO COLOCAR DUAS FORMAS DE ALTERAR A POSTAGEM, UMA DO CRIADOR DO
+	// POST E OUTRA PARA OS OUTROS USUÁRIOS.
 	@ApiOperation(value = "alteração de uma postagem")
 	@PutMapping
-	public ResponseEntity<Postagem> put(@RequestBody Postagem Postagem){
+	public ResponseEntity<Postagem> put(@RequestBody Postagem Postagem) {
 		return ResponseEntity.status(HttpStatus.OK).body(repository.save(Postagem));
 	}
-	
+
+	@PutMapping("doacao/id/{id}/doacao/{doacao}")
+	public ResponseEntity<Postagem> doacaoPostagem(@PathVariable long id, @PathVariable double doacao) {
+		Optional<Postagem> postagem = service.doacao(id, doacao);
+		try {
+			return ResponseEntity.ok(postagem.get());
+		} catch (Exception e) {
+			return ResponseEntity.notFound().build();
+		}
+
+	}
+
 	@ApiOperation(value = "exclui uma postagem")
 	@DeleteMapping("/{id}")
 	public void delete(@PathVariable long id) {
 		repository.deleteById(id);
 	}
-	
+
 }
---
+++ crowdfunding/src/main/java/com/bio/crowdfunding/service/PostagemService.java
@@ -0,0 +1,39 @@
+package com.bio.crowdfunding.service;
+
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.bio.crowdfunding.model.Postagem;
+import com.bio.crowdfunding.repository.PostagemRepository;
+
+@Service
+public class PostagemService {
+	
+	@Autowired
+    private PostagemRepository repository;
+
+    public Optional<Postagem> doacao(long id, double doacao) {
+
+        Optional<Postagem> postagem = repository.findById(id);
+
+        if (postagem.isPresent()) {
+
+            if (postagem.get().getValor_doado() + doacao <= postagem.get().getMeta()) {
+
+                double doacaoSoma = postagem.get().getValor_doado() + doacao;
+
+                postagem.get().setValor_doado(doacaoSoma);
+
+                return Optional.of(repository.save(postagem.get()));
+
+            }
+
+            return Optional.of(postagem.get());
+
+        }
+
+        return null;
+    }
+}
